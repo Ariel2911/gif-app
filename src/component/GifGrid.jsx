adb40cb59ae887fe2b31e180b22897ec45d5308c
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-01 19:33:07 -0300

    Agrega el mensaje 'Cargando...'

--- src/component/GifGrid.jsx
+++ src/component/GifGrid.jsx
@@ -8,6 +8,8 @@ export const GifGrid = ({ category }) => {
     <>
       <h3>{category}</h3>
 
+      {isLoading && <p>Cargando...</p>}
+
       <div className="card-grid">
         {images.map(({ id, title, url }) => (
           <GifGridItem key={id} title={title} url={url} />
