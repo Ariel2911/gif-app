56a43755ff78b4201f4c2e671b6b9cf50ecd9473
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-04 10:13:37 -0300

    Realiza prueba en getGifs(): debe de retornar un arreglo que contenga como mínimo un objeto con: id, title, url

--- test/helpers/getGifs.test.js
+++ test/helpers/getGifs.test.js
@@ -8,4 +8,17 @@ describe('Pruebas en getGifs()', () => {
     expect(Array.isArray(gifs)).toBe(true)
 
   });
+
+  test('debe de retornar un arreglo que contenga como mínimo un objeto con: id, title, url', async() => {
+    const gifs = await getGif('One Punch');    
+    
+    expect(gifs.length).toBeGreaterThan(0)
+
+    expect(gifs[0]).toEqual({
+      id: expect.any( String ),
+      title: expect.any( String ),
+      url: expect.any( String )
+    })
+
+  });
 });
