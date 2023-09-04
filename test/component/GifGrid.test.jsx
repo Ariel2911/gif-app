0d4c3d4d241b357b2396927cdd2ffefc48cf2add
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-04 16:03:29 -0300

    Agrega useFetchGifs en el test de <GifGrid />

--- test/component/GifGrid.test.jsx
+++ test/component/GifGrid.test.jsx
@@ -1,7 +1,15 @@
 import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/component';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Pruebas en <GifGrid />', () => {
+  useFetchGifs.mockReturnValue({
+    images: [],
+    isLoading: true,
+  });
+
   const category = 'One Punch';
 
   test('debe mostrar el loaging y las categorias inicialmente', () => {
