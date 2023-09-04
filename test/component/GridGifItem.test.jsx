5a8ef574beceff3c0d58346054b96b22942a1e0a
Ariel Barrios <ariel29111978@hotmail.com>
2023-09-04 08:51:37 -0300

    Realiza prueba en <GridGifItem />: debe de mostrar la imagen con el URL y el ALT indicado

--- test/component/GridGifItem.test.jsx
+++ test/component/GridGifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { GifGridItem } from '../../src/component';
 
 describe('Pruebas en <GridGifItem />', () => {
@@ -9,5 +9,19 @@ describe('Pruebas en <GridGifItem />', () => {
     const { container } = render(<GifGridItem title={title} url={url} />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  test('debe de mostrar la imagen con el URL y el ALT indicado', () => {
+    render(<GifGridItem title={title} url={url} />);
+
+    //opción 1
+    // expect(screen.getByRole('img').src).toBe(url);
+    // expect(screen.getByRole('img').alt).toBe(title);
+
+    //opción 2
+    const { src, alt } = screen.getByRole('img');
+
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
   });
 });
